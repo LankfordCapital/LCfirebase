3c9c27a0b059c123d9d101749540d59a02062e90
Underwriting Team <underwriting@lankfordcapital.com>
2025-07-25 16:45:38 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/dashboard/application/[program]/page.tsx
+++ src/app/dashboard/application/[program]/page.tsx
@@ -1,3 +1,6 @@
+
+'use client';
+
 import { Suspense } from 'react';
 import { LoanApplicationClient } from '@/components/loan-application-client';
 import { Skeleton } from '@/components/ui/skeleton';
