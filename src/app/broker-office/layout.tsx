9138ab681470ed106946a53524ddf1fe1e6f8885
Underwriting Team <underwriting@lankfordcapital.com>
2025-07-29 04:27:04 +0000

    Now the all of the dashboards need to be secured so that only registered

--- src/app/broker-office/layout.tsx
+++ src/app/broker-office/layout.tsx
@@ -2,12 +2,16 @@
 
 'use client';
 
+import { ProtectedRoute } from "@/components/protected-route";
+
 export default function BrokerOfficeLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-      <div className="bg-primary/5 min-h-screen">{children}</div>
+      <ProtectedRoute redirectTo="/auth/broker-signin">
+        <div className="bg-primary/5 min-h-screen">{children}</div>
+      </ProtectedRoute>
   );
 }
