8a023a3d1a140e72a1608fd617a4c24d7921b0fd
James White <james@lankfordcapital.com>
2025-07-29 15:52:07 +0000

    please make the text in capital less bold

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,8 +3,8 @@ import { cn } from '@/lib/utils';
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <div className={cn("flex items-baseline gap-1 font-headline text-2xl font-bold tracking-tight", className)}>
-      <span className="text-primary">
+    <div className={cn("flex items-baseline gap-1 font-headline text-2xl tracking-tight", className)}>
+      <span className="text-primary font-bold">
         Lankford
       </span>
       <span className="text-foreground">
