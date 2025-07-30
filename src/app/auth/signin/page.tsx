077cb7a8333bcacc86e10cfa11df1edb10beb155
Underwriting Team <underwriting@lankfordcapital.com>
2025-07-30 14:05:04 +0000

    is it possible to make the color behind the icon uniform with the rest o

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -90,7 +90,7 @@ export default function SignInPage() {
             </form>
         </Card>
       </div>
-      <div className="hidden bg-primary/5 lg:flex items-center justify-center p-10">
+      <div className="hidden lg:flex items-center justify-center p-10">
         <Image
           src="https://firebasestorage.googleapis.com/v0/b/lankford-lending.firebasestorage.app/o/Lankford%20Capital%20Icon%20Mark%20Gold.png?alt=media&token=a7a05b83-1979-43a4-a431-511e4d8b71f5"
           alt="Lankford Capital Logo"
