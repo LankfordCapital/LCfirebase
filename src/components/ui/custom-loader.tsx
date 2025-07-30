5fa3f53b783cbba18c21280650d4f35ac48a7110
James White <james@lankfordcapital.com>
2025-07-30 14:20:50 +0000

    it still is not centered

--- src/components/ui/custom-loader.tsx
+++ src/components/ui/custom-loader.tsx
@@ -3,15 +3,17 @@ import Image from "next/image";
 
 export function CustomLoader({ className }: { className?: string }) {
   return (
-    <div className={cn("relative flex items-center justify-center h-6 w-6", className)}>
-      <div className="absolute h-full w-full border-2 border-transparent border-t-primary rounded-full animate-spin"></div>
-      <Image 
-        src="https://firebasestorage.googleapis.com/v0/b/lankford-lending.firebasestorage.app/o/Lankford%20Capital%20Icon%20Mark%20Gold.png?alt=media&token=a7a05b83-1979-43a4-a431-511e4d8b71f5" 
-        alt="Loading" 
-        width={20} 
-        height={20} 
-        className="h-5 w-5"
-      />
+    <div className={cn("relative h-6 w-6", className)}>
+      <div className="absolute inset-0 border-2 border-transparent border-t-primary rounded-full animate-spin"></div>
+      <div className="absolute inset-0 flex items-center justify-center">
+        <Image 
+          src="https://firebasestorage.googleapis.com/v0/b/lankford-lending.firebasestorage.app/o/Lankford%20Capital%20Icon%20Mark%20Gold.png?alt=media&token=a7a05b83-1979-43a4-a431-511e4d8b71f5" 
+          alt="Loading" 
+          width={16} 
+          height={16}
+          className="h-4 w-4"
+        />
+      </div>
     </div>
   );
 }
