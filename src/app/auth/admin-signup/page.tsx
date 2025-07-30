860416dce8dbcd1872088aeac3edc48e98c74858
James White <james@lankfordcapital.com>
2025-07-30 14:55:32 +0000

    i dont know what to do

--- src/app/auth/admin-signup/page.tsx
+++ src/app/auth/admin-signup/page.tsx
@@ -71,7 +71,7 @@ export default function AdminSignUpPage() {
 
   return (
     <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2">
-      <div className="flex items-center justify-center p-4">
+      <div className="hidden bg-primary/5 lg:flex items-center justify-center">
         <Link href="/">
             <Image
             src="https://firebasestorage.googleapis.com/v0/b/lankford-lending.firebasestorage.app/o/Lankford%20Capital%20Icon%20Mark%20Gold.png?alt=media&token=a7a05b83-1979-43a4-a431-511e4d8b71f5"
