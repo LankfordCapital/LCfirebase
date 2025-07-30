424c11239cf53a7f29de8b2472fcd3095cb041b0
James White <james@lankfordcapital.com>
2025-07-30 14:11:06 +0000

    can you link the home page to the lion icon by clicking it for users?

--- src/app/auth/admin-signup/page.tsx
+++ src/app/auth/admin-signup/page.tsx
@@ -110,13 +110,15 @@ export default function AdminSignUpPage() {
         </Card>
       </div>
        <div className="hidden lg:flex items-center justify-center p-10">
-        <Image
-          src="https://firebasestorage.googleapis.com/v0/b/lankford-lending.firebasestorage.app/o/Lankford%20Capital%20Icon%20Mark%20Gold.png?alt=media&token=a7a05b83-1979-43a4-a431-511e4d8b71f5"
-          alt="Lankford Capital Logo"
-          width={256}
-          height={256}
-          className="h-auto w-64 animate-spin-y"
-        />
+        <Link href="/">
+          <Image
+            src="https://firebasestorage.googleapis.com/v0/b/lankford-lending.firebasestorage.app/o/Lankford%20Capital%20Icon%20Mark%20Gold.png?alt=media&token=a7a05b83-1979-43a4-a431-511e4d8b71f5"
+            alt="Lankford Capital Logo"
+            width={256}
+            height={256}
+            className="h-auto w-64 animate-spin-y"
+          />
+        </Link>
       </div>
     </div>
   )
