a4af03e38b0b713390307073a31ccdb683df18d1
James White <james@lankfordcapital.com>
2025-07-30 15:53:53 +0000

    on the start new loan application page i would like to expand the page t

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -17,8 +17,10 @@ export default function DashboardLayout({
     // <ProtectedRoute>
       <div className="flex flex-col min-h-screen">
           <BorrowerDashboardHeader />
-          <main className="flex-1">
-            {children}
+          <main className="flex-1 bg-primary/5">
+            <div className="p-4 md:p-6 lg:p-8">
+              {children}
+            </div>
           </main>
       </div>
     // </ProtectedRoute>
