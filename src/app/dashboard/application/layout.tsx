82c7b5da834a538af768f6b879fde1bd6403bacc
James White <james@lankfordcapital.com>
2025-07-31 14:38:50 +0000

    can you fix the formatting on this page, it is a little tight to the hor

--- src/app/dashboard/application/layout.tsx
+++ src/app/dashboard/application/layout.tsx
@@ -8,7 +8,9 @@ export default function ApplicationLayout({
 }) {
   return (
       <div className="p-4 md:p-6 lg:p-8">
-          {children}
+          <div className="max-w-4xl mx-auto">
+            {children}
+          </div>
       </div>
   );
 }
