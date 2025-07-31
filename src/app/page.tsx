656345def2b97ade9c26070d781f7f6dbafdfec0
James White <james@lankfordcapital.com>
2025-07-31 18:47:23 +0000

    please move the lion under "get started" and "explore loans" to the bott

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -114,16 +114,14 @@ export default function Home() {
               <Link href="/lending/commercial">Explore Loans</Link>
             </Button>
           </div>
-           <div className="mt-8">
-            <Image 
-              src="https://firebasestorage.googleapis.com/v0/b/lankford-lending.firebasestorage.app/o/Lankford%20Capital%20Icon%20Mark%20Gold.png?alt=media&token=a7a05b83-1979-43a4-a431-511e4d8b71f5" 
-              alt="Lankford Capital Icon"
-              width={64}
-              height={64}
-              className="mx-auto"
-            />
-          </div>
         </div>
+        <Image 
+          src="https://firebasestorage.googleapis.com/v0/b/lankford-lending.firebasestorage.app/o/Lankford%20Capital%20Icon%20Mark%20Gold.png?alt=media&token=a7a05b83-1979-43a4-a431-511e4d8b71f5" 
+          alt="Lankford Capital Icon"
+          width={64}
+          height={64}
+          className="absolute bottom-8 right-8 z-20"
+        />
       </section>
 
       <section id="products" className="py-16 md:py-24 bg-background">
