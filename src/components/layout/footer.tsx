eab4f2da879d8a84ae4ba65b3edb263bc8483cf3
James White <james@lankfordcapital.com>
2025-08-01 14:18:45 +0000

    please move the lion icon to the bottom of the page and center

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -72,7 +72,6 @@ export function Footer() {
 
         <div className="border-t pt-6 flex flex-col items-center justify-center gap-4">
           <div className="flex items-center gap-2 text-sm text-muted-foreground">
-                <Image src="https://firebasestorage.googleapis.com/v0/b/lankford-lending.firebasestorage.app/o/Lankford%20Capital%20Icon%20Mark%20Gold.png?alt=media&token=a7a05b83-1979-43a4-a431-511e4d8b71f5" alt="Lankford Capital Icon" width={30} height={30} />
                 <span>&copy; {new Date().getFullYear()} Lankford Capital. All rights reserved.</span>
           </div>
            <div className="flex items-center space-x-1">
@@ -91,6 +90,9 @@ export function Footer() {
                   <Facebook className="h-5 w-5" />
                 </a>
               </Button>
+            </div>
+            <div className="flex justify-center mt-4">
+                <Image src="https://firebasestorage.googleapis.com/v0/b/lankford-lending.firebasestorage.app/o/Lankford%20Capital%20Icon%20Mark%20Gold.png?alt=media&token=a7a05b83-1979-43a4-a431-511e4d8b71f5" alt="Lankford Capital Icon" width={30} height={30} />
             </div>
         </div>
       </div>
