74909010ea54ad0048fcb5ba1407b7c7c466683f
James White <james@lankfordcapital.com>
2025-08-07 00:30:38 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -105,7 +105,7 @@ export default function Header() {
               </NavigationMenuItem>
               {navLinks.map((link) => (
                  <NavigationMenuItem key={link.href}>
-                    <Link href={link.href} asChild>
+                    <Link href={link.href} legacyBehavior passHref>
                       <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                         {link.label}
                       </NavigationMenuLink>
