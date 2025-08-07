f3de441c0ef5dba22089d745d05af13165f01ec5
James White <james@lankfordcapital.com>
2025-08-07 15:47:25 +0000

    but we need the NOO construction page

--- src/app/lending/residential-noo-construction/page.tsx
+++ src/app/lending/residential-noo-construction/page.tsx
@@ -1,6 +1,5 @@
 
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
 import { Check } from "lucide-react";
