e4017ab89868c245c5a8bc2bda15449aa79f46a0
Underwriting Team <underwriting@lankfordcapital.com>
2025-08-29 15:59:32 +0000

    please correct the edges of the window, they are too close please add pr

--- src/app/broker-office/layout.tsx
+++ src/app/broker-office/layout.tsx
@@ -12,7 +12,7 @@ export default function BrokerOfficeLayout({
   return (
         <ProtectedRoute allowedRoles={['broker', 'workforce', 'admin']} redirectTo="/auth/signin">
           <div className="flex flex-col min-h-screen">
-            <main>
+            <main className="p-4 md:p-6 lg:p-8">
               {children}
             </main>
           </div>
